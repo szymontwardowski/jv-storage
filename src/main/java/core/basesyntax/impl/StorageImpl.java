3a1a50a1cd539146bf1aad73df62bdac431d4d4b
Szymon Twardowski <szytwardowski@gmail.com>
2025-09-03 22:41:21 +0200

    Pair

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,33 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    @Override
+
+    private Pair<K, V>[] items = new Pair[10];
+    private int count = 0;
+
     public void put(K key, V value) {
+        for (int i = 0; i < count; i++) {
+            if (items[i].getKey().equals(key)) {
+                items[i].setValue(value);
+                return;
+            }
+        }
+
+        items[count] = new Pair<>(key, value);
+        count++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < count; i++)
+            if (items[i].getKey().equals(key)) {
+                return items[i].getValue();
+            }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return count;
     }
 }
